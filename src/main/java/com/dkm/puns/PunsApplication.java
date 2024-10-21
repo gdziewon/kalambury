d3dd5b4c761edf522fdbe6e0fd900085918c61a9
Hubert <kordula.hubert@gmail.com>
2024-10-21 20:58:50 +0200

    Initial commit
    Set up Spring Boot project.

---
+++ src/main/java/com/dkm/puns/PunsApplication.java
@@ -0,0 +1,13 @@
+package com.dkm.puns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PunsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PunsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dkm/puns/PunsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dkm.puns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PunsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
